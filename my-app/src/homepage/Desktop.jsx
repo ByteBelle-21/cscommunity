141a0a83b99912015f1ab1e0b093916b03e3f0b0
ByteBelle-21 <jrg814@usask.ca>
2024-08-17 00:34:03 -0400

    Integrated figma design into project

---
+++ my-app/src/homepage/Desktop.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import "./style.css";
+
+export const Desktop = () => {
+  return (
+    <div className="desktop">
+      <div className="div">
+        <img className="vector" alt="Vector" src="/img/vector-7.png" />
+        <img className="img" alt="Vector" src="/img/vector-15.svg" />
+        <img className="vector-2" alt="Vector" src="/img/vector-17.svg" />
+        <img className="vector-3" alt="Vector" src="/img/vector-18.svg" />
+        <img className="vector-4" alt="Vector" src="/img/vector-19.svg" />
+        <img className="vector-5" alt="Vector" src="/img/vector-20.svg" />
+        <img className="vector-6" alt="Vector" src="/img/vector-21.svg" />
+        <img className="vector-7" alt="Vector" src="/img/vector-22.svg" />
+        <img className="vector-8" alt="Vector" src="/img/vector-23.svg" />
+        <img className="vector-9" alt="Vector" src="/img/vector-24.svg" />
+        <img className="vector-10" alt="Vector" src="/img/vector-25.svg" />
+        <img className="vector-11" alt="Vector" src="/img/vector-26.svg" />
+        <img className="vector-12" alt="Vector" src="/img/vector-27.svg" />
+        <img className="vector-13" alt="Vector" src="/img/vector-28.svg" />
+        <div className="group" />
+        <img className="group-2" alt="Group" src="/img/group-31.png" />
+        <div className="overlap">
+          <img className="vector-14" alt="Vector" src="/img/vector.svg" />
+          <img className="vector-15" alt="Vector" src="/img/vector-1.svg" />
+          <img className="vector-16" alt="Vector" src="/img/vector-2.svg" />
+          <div className="frame">
+            <div className="group-wrapper">
+              <img className="group-3" alt="Group" src="/img/group-144.png" />
+            </div>
+            <div className="img-wrapper">
+              <img className="group-4" alt="Group" src="/img/group-16.png" />
+            </div>
+            <img className="group-5" alt="Group" src="/img/group-147.png" />
+            <img className="group-6" alt="Group" src="/img/group-148.png" />
+          </div>
+        </div>
+        <img className="vector-17" alt="Vector" src="/img/vector-3.svg" />
+        <img className="group-7" alt="Group" src="/img/group-33.png" />
+        <div className="group-8" />
+        <div className="overlap-group-wrapper">
+          <div className="overlap-group">
+            <div className="ellipse" />
+            <img className="group-9" alt="Group" src="/img/group-56.png" />
+          </div>
+        </div>
+        <div className="overlap-2">
+          <div className="rectangle" />
+          <img className="group-10" alt="Group" src="/img/group-134.png" />
+          <div className="rectangle-2">  </div>
+        </div>
+        <img className="exclude" alt="Exclude" src="/img/exclude.png" />
+      </div>
+    </div>
+  );
+};
