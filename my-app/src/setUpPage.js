37469c48c03faf6412ad88cc951d1de6447eff3f
ByteBelle-21 <jrg814@usask.ca>
2024-08-19 19:15:48 -0400

    Editing profile setup page

---
+++ my-app/src/setUpPage.js
@@ -0,0 +1,21 @@
+import './setUpPage.css';
+import Container from 'react-bootstrap/Container'; 
+
+function SetUpPage(){
+    return(
+        <div className="setuppage vertical-placement">
+            <h5>Customize Your Profile:Let Your Expertise Shine and Build Trust with the Community!</h5>
+            <Container className='main-container horizontal-placement'>
+                <Container className='image-field vertical-placement'>
+                    ,wjbfjkqwbfkwnbkfbjhfbwh
+                </Container>
+                <Container className='form-field vertical-placement'>
+                    ehncfkwhjefkur
+                </Container>
+            </Container>
+
+        </div>
+    )
+}
+
+export default SetUpPage;
