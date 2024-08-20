3726934a1b0bd58ebcd96997598563e9895a14a8
ByteBelle-21 <jrg814@usask.ca>
2024-08-20 05:33:08 -0400

    editing allChannels page

--- my-app/src/allChannels.js
+++ my-app/src/allChannels.js
@@ -7,12 +7,26 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import { NavLink } from 'react-router-dom';
+import Button from 'react-bootstrap/esm/Button';
+import { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
 
 
 function AllChannels(){
+    const [createChannelform, setCreateChannelForm] = useState(false)
+    const openCreationForm = ()=>{
+        setCreateChannelForm(true)
+    }
+    const closeCreationForm=()=>{
+        setCreateChannelForm(false)
+    }
+
+
+
+
     return(
         <div className="allChannels-page">
-            <Stack direction="horizontal" gap={3} className="full-width-bands">
+            <Stack direction="horizontal" gap={3} className="full-width-bands" style={{height:'5vh'}}>
                 <Nav.Link href="#" className="me-auto mx-4">CScommunity</Nav.Link>
                 <Nav.Link className='horizontal-placement'>Profile</Nav.Link>
                 <Nav.Link href="#" className="mx-4" >Log Out</Nav.Link>
@@ -26,13 +40,63 @@ function AllChannels(){
                     </Form> 
                 </div>       
             </div>
-            <div className='horizontal-placement m-4 '>
+            <div className='horizontal-placement' style={{margin:'2vh'}}>
                 <Container className='channels-list'>
 
                 </Container>
-                <Container className='rightside-bar '>
-                    <h6>Direct Messages</h6>
-                    <Container className='direct-messages vertical-placement'>
+                <Container className='rightside-bar vertical-placement '>  
+                    <Container className='direct-messages '>
+                        <h6>Direct Messages</h6>
+                        <div className='direct-msg-member'>
+                            <Stack direction="horizontal" gap={3}>
+                                <img src="/Group 205.png"  style={{height:'2vw'}}/>
+                                <div className=' me-auto'>
+                                    Username
+                                    <Nav.Link style={{fontSize:'small'}} >View Profile</Nav.Link>
+                                </div>
+                                <Nav.Link style={{fontSize:'small'}} >View Conversation</Nav.Link>
+                            </Stack>
+                        </div>
+                        <div className='direct-msg-member'>
+                            <Stack direction="horizontal" gap={3}>
+                                <img src="/Group 205.png"  style={{height:'2vw'}}/>
+                                <div className=' me-auto'>
+                                    Username
+                                    <Nav.Link style={{fontSize:'small'}} >View Profile</Nav.Link>
+                                </div>
+                                <Nav.Link style={{fontSize:'small'}} >View Conversation</Nav.Link>
+                            </Stack>
+                        </div>
+                        <div className='direct-msg-member'>
+                            <Stack direction="horizontal" gap={3}>
+                                <img src="/Group 205.png"  style={{height:'2vw'}}/>
+                                <div className=' me-auto'>
+                                    Username
+                                    <Nav.Link style={{fontSize:'small'}} >View Profile</Nav.Link>
+                                </div>
+                                <Nav.Link style={{fontSize:'small'}} >View Conversation</Nav.Link>
+                            </Stack>
+                        </div>
+                        <div className='direct-msg-member'>
+                            <Stack direction="horizontal" gap={3}>
+                                <img src="/Group 205.png"  style={{height:'2vw'}}/>
+                                <div className=' me-auto'>
+                                    Username
+                                    <Nav.Link style={{fontSize:'small'}} >View Profile</Nav.Link>
+                                </div>
+                                <Nav.Link style={{fontSize:'small'}} >View Conversation</Nav.Link>
+                            </Stack>
+                        </div>
+                        <div className='direct-msg-member'>
+                            <Stack direction="horizontal" gap={3}>
+                                <img src="/Group 205.png"  style={{height:'2vw'}}/>
+                                <div className=' me-auto'>
+                                    Username
+                                    <Nav.Link style={{fontSize:'small'}} >View Profile</Nav.Link>
+                                </div>
+                                <Nav.Link style={{fontSize:'small'}} >View Conversation</Nav.Link>
+                            </Stack>
+                        </div>
                         <div className='direct-msg-member'>
                             <Stack direction="horizontal" gap={3}>
                                 <img src="/Group 205.png"  style={{height:'2vw'}}/>
@@ -64,14 +128,23 @@ function AllChannels(){
                             </Stack>
                         </div>
                     </Container>
-                    <Stack direction="horizontal" gap={3} className='create-channel-stack'>
-                            <img src="/Group 205.png"  style={{height:'2vw'}}/>
-                            <div className=' me-auto'>
-                                Username
-                                <Nav.Link style={{fontSize:'small'}} >View Profile</Nav.Link>
-                            </div>
-                                <Nav.Link style={{fontSize:'small'}} >View Conversation</Nav.Link>
-                    </Stack>               
+                    
+                    <div className='create-channel-stack vertical-placement '>
+                        <img src="/Group 209.png" />
+                        <Button className='create-channel-btn' onClick={openCreationForm}>Create new Channel</Button>
+                    </div>
+                    <Modal  show={createChannelform} onHide={closeCreationForm} centered style={{"--bs-modal-border-radius":'1vw'}} >
+                        <Modal.Body className='vertical-placement'>
+                            <h5>Create your Own Channel</h5>
+                            <Form className='mt-2'>
+                                <Form.Group controlId="signup-username" >
+                                    <Form.Label>Enter Channel Name</Form.Label>
+                                    <Form.Control type="text" placeholder="i.e Java discussion channel" className='mb-3'/>
+                                </Form.Group>
+                                <Button>Create Channel</Button>
+                            </Form>
+                        </Modal.Body>
+                    </Modal>                                
                 </Container>
             </div>
         </div>
